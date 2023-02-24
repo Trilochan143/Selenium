9474ed53900fbf3b19add91a1ab8a2f1c50fe865
Trilochan <dev1@gmail.com>
2023-02-24 15:01:16 +0530

    m1 method added

--- Tutorial/src/GitHUBTest.java
+++ Tutorial/src/GitHUBTest.java
@@ -5,7 +5,10 @@ public class GitHUBTest {
 	{
 		System.out.print("from m1 of office");
 	}
-	
+	public void m2()
+	{
+		System.out.print("from m1 of office");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
