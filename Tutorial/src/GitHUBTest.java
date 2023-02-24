cc968838843c9a930c72cb77f460ec74cd5c3d2f
Trilochan <dev1@gmail.com>
2023-02-24 14:52:59 +0530

    Added GitHubTest.java

---
+++ Tutorial/src/GitHUBTest.java
@@ -0,0 +1,17 @@
+
+public class GitHUBTest {
+
+	public void m1()
+	{
+		System.out.print("from m1 of office");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println();
+		
+
+	}
+
+}
